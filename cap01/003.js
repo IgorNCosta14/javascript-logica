ec2ab42e66a01a5aad6cd8c35d6238121da3852b
Igor Nascimento Costa <69372874+IgorNCosta14@users.noreply.github.com>
2022-11-07 21:24:00 -0300

    cap 01

---
+++ cap01/003.js
@@ -0,0 +1,14 @@
+function funcaoQualquer() {
+
+    var n1 = 10;
+    var n2 = 20;
+    var n3 = 30;
+
+    return n2
+;}
+
+var retorno = funcaoQualquer();
+
+document.prompt
+
+console.log(retorno);
